be8b2995dd8c14e8f58cbd6562e77b2a64b9630f
Eric Dodgion <Barry.manowar@gmail.com>
2021-05-21 19:45:09 -0600

    events

--- models/Project.js
+++ models/Project.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Project extends Model {}
+class Event extends Model {}
 
-Project.init(
+Event.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -40,8 +40,8 @@ Project.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'project',
+    modelName: 'event',
   }
 );
 
-module.exports = Project;
+module.exports = Event;
